3c219ad54b99a16217fd0c4572beb855bd52f103
amruhashim <78436613+amruhashim@users.noreply.github.com>
2025-06-10 01:32:04 +0500

    Update plugins.lua

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,76 +1,92 @@
-vim.cmd([[packadd packer.nvim]])
-return require("packer").startup(function(use)
-    -- Packer can manage itself
-    use("wbthomason/packer.nvim")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
     -- Telescope with dependencies
-    use({
+    {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.8",
-        requires = {
-            { "nvim-lua/plenary.nvim" },
-            { "nvim-tree/nvim-web-devicons" }, -- File icons
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
         },
-    })
-    -- Telescope FZF native for better performance
-    use({
+    },
+    {
         "nvim-telescope/telescope-fzf-native.nvim",
-        run = "make",
-    })
-    -- File icons (can be used by other plugins too)
-    use("nvim-tree/nvim-web-devicons")
-    use("rebelot/kanagawa.nvim")
-    use({
+        build = "make",
+    },
+
+    "nvim-tree/nvim-web-devicons",
+    "rebelot/kanagawa.nvim",
+
+    {
         "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-    })
-    use("nvim-treesitter/playground")
-    use("stevearc/conform.nvim")
-    use("nvim-lua/plenary.nvim")
-    use("theprimeagen/harpoon")
-    use("mbbill/undotree")
-    use("tpope/vim-fugitive")
-    use({
+        build = ":TSUpdate",
+    },
+    "nvim-treesitter/playground",
+    "stevearc/conform.nvim",
+    "nvim-lua/plenary.nvim",
+    "theprimeagen/harpoon",
+    "mbbill/undotree",
+    "tpope/vim-fugitive",
+
+    {
         "VonHeikemen/lsp-zero.nvim",
-        requires = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" },
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
-
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-path" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-nvim-lua" },
-
-            -- Snippets
-            { "L3MON4D3/LuaSnip" },
-            { "rafamadriz/friendly-snippets" },
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lua",
+            "L3MON4D3/LuaSnip",
+            "rafamadriz/friendly-snippets",
         }
-    })
-    use({
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    })
-    use({
-        'akinsho/bufferline.nvim',
-        tag = "*",
-        requires = 'nvim-tree/nvim-web-devicons'
-    })
-    use {
-        'nvzone/typr',
-        requires = 'nvzone/volt',
+    },
+
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" }
+    },
+
+    {
+        "akinsho/bufferline.nvim",
+        version = "*",
+        dependencies = "nvim-tree/nvim-web-devicons"
+    },
+
+    {
+        "nvzone/typr",
+        dependencies = "nvzone/volt",
         config = function()
             require('typr').setup({})
         end,
-        cmd = { 'Typr', 'TyprStats' }
-    }
-    use('ggandor/leap.nvim')
-    use({
-        'stevearc/oil.nvim',
-        requires = 'nvim-tree/nvim-web-devicons',
+        cmd = { "Typr", "TyprStats" }
+    },
+
+    "ggandor/leap.nvim",
+
+    {
+        "stevearc/oil.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
         config = function()
             require('oil').setup({
                 view_options = {
@@ -78,11 +94,12 @@ return require("packer").startup(function(use)
                 }
             })
         end
-    })
-    use({
-        'numToStr/Comment.nvim',
+    },
+
+    {
+        "numToStr/Comment.nvim",
         config = function()
             require('Comment').setup()
         end
-    })
-end)
+    },
+})
