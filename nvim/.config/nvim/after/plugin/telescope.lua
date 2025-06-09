6228d322a4b165652845500cff6464c6618c26b2
amruhashim <78436613+amruhashim@users.noreply.github.com>
2025-06-10 04:06:52 +0500

    Update telescope.lua

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,36 @@
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
 
--- Setup telescope with fzf-native
+-- Setup telescope with fzf-native and custom mappings
 require('telescope').setup({
+    defaults = {
+        mappings = {
+            i = {
+                -- Insert mode navigation (for when typing/filtering)
+                ["<C-n>"] = actions.move_selection_next,
+                ["<C-p>"] = actions.move_selection_previous,
+                ["<Down>"] = actions.move_selection_next,
+                ["<Up>"] = actions.move_selection_previous,
+                ["<Tab>"] = actions.move_selection_next,
+                ["<S-Tab>"] = actions.move_selection_previous,
+                -- Disable the problematic ones
+                ["<C-j>"] = false,
+                ["<C-k>"] = false,
+            },
+            n = {
+                -- Normal mode navigation (j/k work here!)
+                ["j"] = actions.move_selection_next,
+                ["k"] = actions.move_selection_previous,
+                ["<Down>"] = actions.move_selection_next,
+                ["<Up>"] = actions.move_selection_previous,
+                ["gg"] = actions.move_to_top,
+                ["G"] = actions.move_to_bottom,
+                -- Switch back to insert mode for filtering
+                ["i"] = function() vim.cmd("startinsert") end,
+                ["a"] = function() vim.cmd("startinsert!") end,
+            },
+        },
+    },
     extensions = {
         fzf = {}
     }
@@ -20,7 +49,7 @@ end)
 -- Package search keymap
 vim.keymap.set('n', '<leader>ep', function()
     builtin.find_files({
-        cwd = vim.fn.stdpath('data') .. '/packer',
+        cwd = vim.fn.stdpath('data') .. '/lazy',
         prompt_title = 'Edit Packages'
     })
 end, { desc = 'Search plugin files' })
@@ -28,7 +57,7 @@ end, { desc = 'Search plugin files' })
 -- Find files including hidden
 vim.keymap.set('n', '<leader>ph', function()
     builtin.find_files({ hidden = true })
-end, { desc = 'Find files (including hidden)' })
+end, { desc = 'Find files including hidden' })
 
 -- Find directories only
 vim.keymap.set('n', '<leader>pd', function()
@@ -44,4 +73,10 @@ vim.keymap.set('n', '<leader>pD', function()
         find_command = { 'fd', '--type', 'd', '--hidden' },
         prompt_title = 'Find Directories (Hidden)'
     })
-end, { desc = 'Find directories (including hidden)' })
+end, { desc = 'Find directories including hidden' })
+
+-- Diagnostics keymaps
+vim.keymap.set('n', '<leader>pe', builtin.diagnostics, { desc = 'Project errors' })
+vim.keymap.set('n', '<leader>fe', function()
+    builtin.diagnostics({ bufnr = 0 })
+end, { desc = 'File errors' })
